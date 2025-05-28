5b8b42685ed03952c56d49a9c0c46de429ef559c
Rodrigo Borges <rodrigo.borges@empregare.com.br>
2025-05-28 19:52:23 -0300

    implementação do middleware para os logs

--- FIAPCloudGames.API/Extensions/ServiceExtensions.cs
+++ FIAPCloudGames.API/Extensions/ServiceExtensions.cs
@@ -94,7 +94,7 @@ public static class ServiceExtensions
         }
 
         app.UseHttpsRedirection();
-
+        app.UseCustomExceptionHandling();
         app.UseAuthentication();
         app.UseAuthorization();
     }
